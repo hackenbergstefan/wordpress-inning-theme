cc0eed89a7fb1704a3e763f708c33493aa73e506
hackenbergstefan <hackenberg.stefan@t-online.de>
2015-05-16 18:56:15 +0200

    posts page

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 
 	<main role="main">
+        <div class="page-title"><?php _e( 'Latest Posts', 'html5blank' ); ?></div>
+
 		<!-- section -->
 		<section>
-
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
 			<?php get_template_part('loop'); ?>
 
