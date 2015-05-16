3beeb9f60c7ad8b4f98cdbbbfcf48b4f7cfa99be
hackenbergstefan <hackenberg.stefan@t-online.de>
2015-05-16 18:43:39 +0200

    add section correctly ;)

--- template-sitemap.php
+++ template-sitemap.php
@@ -1,8 +1,8 @@
 <?php /* Template Name: Sitemap Template */ get_header(); ?>
 
 <main role="main">
+    <div class="page-title"><?php the_title(); ?></div>
     <section>
-        <div class="page-title"><?php the_title(); ?></div>
         <div id="sitemap">
             <?php generate_sitemap(['Kontakt', 'Impressum']); ?>
         </div>
