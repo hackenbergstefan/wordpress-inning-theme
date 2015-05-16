3e1863d483a76546d587dfc1cf369d57328b5e7d
hackenbergstefan <hackenberg.stefan@t-online.de>
2015-05-16 18:42:39 +0200

    add section

--- template-sitemap.php
+++ template-sitemap.php
@@ -1,10 +1,12 @@
 <?php /* Template Name: Sitemap Template */ get_header(); ?>
 
 <main role="main">
-    <div class="page-title"><?php the_title(); ?></div>
-    <div id="sitemap">
-        <?php generate_sitemap(['Kontakt', 'Impressum']); ?>
-    </div>
+    <section>
+        <div class="page-title"><?php the_title(); ?></div>
+        <div id="sitemap">
+            <?php generate_sitemap(['Kontakt', 'Impressum']); ?>
+        </div>
+    </section>
 </main>
 
 <?php get_footer(); ?>
