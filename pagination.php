f14f6aedbb1da0e98eec909244978183b9141bbf
hackenbergstefan <hackenberg.stefan@t-online.de>
2015-05-17 17:31:29 +0200

    pagination only if there are pages

--- pagination.php
+++ pagination.php
@@ -1,5 +1,7 @@
-<!-- pagination -->
-<div class="pagination">
-	<?php html5wp_pagination(); ?>
-</div>
-<!-- /pagination -->
+<?php if( $wp_query->max_num_pages > 1 ): ?>
+    <!-- pagination -->
+    <div class="pagination">
+        <?php html5wp_pagination(); ?>
+    </div>
+    <!-- /pagination -->
+<?php endif; ?>
